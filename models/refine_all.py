50580660bec7b6a6f4192d7731cc5d66a43d5c80
federaspa <federico.raspanti2000@gmail.com>
2024-07-24 16:47:19 +0200

    more finetuning

--- models/refine_all.py
+++ models/refine_all.py
@@ -5,24 +5,24 @@ import sys
 from self_refinement import SelfRefinementEngine, GrammarConstrainedModel
 
 sketchers = [
-    'gpt-3.5-turbo',
+    # 'gpt-3.5-turbo',
     # 'gpt-4-turbo',
     'gpt-4o'
 ]
 refiners = [
-    'GCD/llms/llama-2-7b.Q6_K.gguf',
-    'GCD/llms/llama-2-13b.Q6_K.gguf',
-    'GCD/llms/upstage-llama-2-70b-instruct-v2.Q6_K.gguf',
-    'GCD/llms/Meta-Llama-3-8B-Instruct.Q6_K.gguf',
-    'GCD/llms/Meta-Llama-3-70B-Instruct-v2.Q6_K-00001-of-00002.gguf',
-    'GCD/llms/mistral-7b-instruct-v0.2.Q6_K.gguf',
-    'GCD/llms/mixtral-8x7b-instruct-v0.1.Q6_K.gguf',
+    'GCD/llms/llama-2-7b-finetune.gguf',
+    # 'GCD/llms/llama-2-13b.Q6_K.gguf',
+    # 'GCD/llms/upstage-llama-2-70b-instruct-v2.Q6_K.gguf',
+    # 'GCD/llms/Meta-Llama-3-8B-Instruct.Q6_K.gguf',
+    # 'GCD/llms/Meta-Llama-3-70B-Instruct-v2.Q6_K-00001-of-00002.gguf',
+    # 'GCD/llms/mistral-7b-instruct-v0.2.Q6_K.gguf',
+    # 'GCD/llms/mixtral-8x7b-instruct-v0.1.Q6_K.gguf',
     # 'GCD/llms/Mixtral_8x22B/Q6_K-00001-of-00004.gguf',
-    None
+    # None
 ]
 load_dirs = [
-    # 'outputs_1',
-    'outputs_2',
+    # 'outputs_1_finetune',
+    # 'outputs_2',
     'outputs_3'
 ]
 
